a68c3383ef08630d15bcc8c15bc7b5929e3bac4c
Joshua Couto <joshua.s.couto@gmail.com>
2026-02-18 15:12:05 -0700

    Committing Completed BubbleSort.java File to Remote Repository

--- src/utilities/BubbleSort.java
+++ src/utilities/BubbleSort.java
@@ -7,6 +7,34 @@
 
 package utilities;
 
-public class BubbleSort {
+import java.util.Comparator;
 
+public class BubbleSort implements SortAlgorithm {
+	
+	@Override
+	public <T extends Comparable<T>> void sort(T[] array) {
+		
+		/* Outer Loop controls how many passes we make over the array.
+		 * Each Pass checks one fewer element. */
+	
+		for (int i = 0; i <= array.length - 1; i++) {
+			
+			/* Inner loop compares each adjacent pair of elements.
+			 * Range shrinks each pass since the smallest values
+			 * have already settled at the end of the array. */
+			
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				
+				/* If the current element is smaller than the next,
+				 * they are in the wrong order for descending sort.
+				 * Swap them using a temporary variable. */
+				
+				if (array[j].compareTo(array[j + 1]) < 0) {
+					T temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
 }
