10f21654304b7321aef943e8cc32dc9594d289de
aaron <aareid1999@gmail.com>
2026-02-26 17:58:01 -0700

    added in volume comparator,merge sort, quick sort

--- src/utilities/VolumeComparator.java
+++ src/utilities/VolumeComparator.java
@@ -7,6 +7,16 @@
 
 package utilities;
 
-public class VolumeComparator {
+import java.util.Comparator;
+
+import shapes.Shape;
+
+public class VolumeComparator implements Comparator<Shape>{
+
+	@Override
+	public int compare(Shape o1, Shape o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
